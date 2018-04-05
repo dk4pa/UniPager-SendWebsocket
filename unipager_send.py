479a03f5458000b99bb1631bf9b1282648385df9
Ralf Wilke <ralf@ralfwilke.com>
2018-04-05 16:48:46 +0200

    Added function bits

--- unipager_send.py
+++ unipager_send.py
@@ -6,9 +6,14 @@ import argparse
 
 from websocket import create_connection
 
-#DEBUG = False
+DEBUG = False
 
-parser = argparse.ArgumentParser(description='Set status LEDs')
+def debug( str ):
+	if DEBUG:
+		print str
+	return
+
+parser = argparse.ArgumentParser(description='Send paging call direct via Unipager')
 parser.add_argument('--hostname', default='localhost',
                     help='The host running Unipager, default localhost')
 parser.add_argument('--port', default='8055',
@@ -19,29 +24,31 @@ parser.add_argument('--ric', dest='ric', default=None, type=int,
                     help='RIC to send the message to')
 parser.add_argument('--type', dest='type', default=1,
                     help='0 = Numeric, 1 = Alphanumeric, default 1')
+parser.add_argument('--func', dest='func', default=3,
+                    help='Function Bits in POCSAG datagram, default 3')
 parser.add_argument('--msg', dest='msg', default='',
-                    help='Message')
-#parser.add_argument('--debug', dest='debug', action='store_true',
-#                    help='Enable debug')
+                    help='Message, if containing spaces: "TEXT WITH SPACES"')
+parser.add_argument('--debug', dest='debug', action='store_true',
+                    help='Enable debug')
 
 args = parser.parse_args()
-#DEBUG |= args.debug
-#if DEBUG: print("Debug enabled")
+DEBUG |= args.debug
+if DEBUG: print("Debug enabled")
 
 hostname = args.hostname
 port = args.port
 ric = args.ric
 type = args.type
+func = args.func
 msg = args.msg
 password = args.password
 
 if not msg:
-        print('No message given, nothing to do')
-        exit()
+	print('No message given, nothing to do')
+	exit()
 if not ric:
         print('No RIC given, nothing to do')
         exit()
-
 
 #websocket.enableTrace(True)
 
@@ -53,20 +60,24 @@ ws = create_connection('ws://' + hostname + ":" + port + '/')
 ########################################################################
 
 if type == 0:
-        m_type = "Numeric"
+	m_type = "Numeric"
 elif type == 1:
-        m_type = "AlphaNum"
+	m_type = "AlphaNum"
 else:
-        exit()
+	exit()
 
 ########################################################################
 # Switch Messagefunction Func0, Func1, Func2, Func3
 ########################################################################
 
-#m_func = "Func0"
-#m_func = "Func1"
-#m_func = "Func2"
-m_func = "Func3"
+if func == 0:
+	m_func = "Func0"
+elif func == 1:
+	m_func = "Func1"
+elif func == 2:
+	m_func = "Func2"
+else:
+	m_func = "Func3"
 
 ########################################################################
 # SendMessage with Variables
@@ -74,5 +85,5 @@ m_func = "Func3"
 
 ws.send('{"Authenticate":"' + password + '"}')
 string_to_send = "{\"SendMessage\": {\"addr\": %s, \"data\": \"%s\", \"mtype\": \"%s\", \"func\": \"%s\"}}" % (ric, msg, m_type, m_func)
-#print(string_to_send)
+debug(string_to_send)
 ws.send(string_to_send)
